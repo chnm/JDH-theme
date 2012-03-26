546f39119553c4ecad3ebf35097c1995a747d9e0
Kim Nguyen <design@kimisgold.com>
2012-03-26 16:45:56 -0400

    Some tentative styles for the author page template.

--- author.php
+++ author.php
@@ -4,19 +4,25 @@
 $curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author));
 ?>
 
-<h1>Entries by <?php echo $curauth->first_name; ?> <?php echo $curauth->last_name; ?>.</h1>
+<h1><?php echo $curauth->first_name; ?> <?php echo $curauth->last_name; ?></h1>
 
 <div class="author-bio">
+
+    <h2>About</h2>
 
     <?php echo nl2br($curauth->description); ?>
     
 </div>
 
+<div class="author-posts">
+
+    <h2>Entries</h2>
+
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     
     <div class="post">
     
-        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
         
         <?php the_excerpt(); ?>
         
@@ -26,6 +32,8 @@ $curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : g
     <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
 
 <?php endif; ?>
+
+</div>
 
 <div class="navigation"><?php posts_nav_link('<span class="break"> </span>', "Newer results", "Older results"); ?></div>
 
