499abb380aaf39b318663b431d1d5641b7863ec2
Kim Nguyen <design@kimisgold.com>
2012-09-13 03:24:21 -0400

    Use nicer method for grabbing last category value

--- functions.php
+++ functions.php
@@ -37,7 +37,7 @@ function jdh_nested_subcategories($categoryPosts, $currentPostId, $parentCatId) 
     foreach($categoryPosts as $post) {
         setup_postdata($post);
         $postCategories = get_the_category();
-        $lastCategory = $postCategories[0];
+        $lastCategory = end($postCategories);
         if($lastCategory->term_id == $parentCatId) {
             if($post->ID == $currentPostId) {
                 echo '<li class="current-menu-item"><a href="' . get_permalink() . '">' . $post->post_title . '</a><br>';
